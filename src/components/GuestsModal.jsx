637728eb7d1b5557f299c47ebc5cae19cf64e497
rodrigomoll <victor.mollocondo@tecsup.edu.pe>
2023-10-09 22:09:56 -0500

    cuarto comentario

--- src/components/GuestsModal.jsx
+++ src/components/GuestsModal.jsx
@@ -41,12 +41,12 @@ const GuestsModal = ({ isOpen, onRequestClose, onSearch }) => {
       className='customModal'
     >
       <div className='container mx-auto'>
-        <div className='undefined my-8 sm:ml-1/2'>
-          <h1 className='font-extrabold text-gray-800'>Adults</h1>
-          <span className='text-gray-400'>Age 13 or above</span>
-          <div className='mt-2'>
+        <div className='section'>
+          <h1 className='title'>Adults</h1>
+          <span className='description'>Age 13 or above</span>
+          <div className='contador'>
             <button
-              className='mr-2 relative px-1 rounded-md text-gray-800 border border-gray-400 hover:text-gray-600 focus:border-blue-300 focus:outline-none'
+              className='botonContador'
               onClick={() => decreaseGuests('adults')}
             >
               －
@@ -60,19 +60,19 @@ const GuestsModal = ({ isOpen, onRequestClose, onSearch }) => {
             </button>
           </div>
         </div>
-        <div className='undefined my-8 sm:ml-1/2'>
-          <h1 className='font-extrabold text-gray-800'>Children</h1>
-          <span className='text-gray-400'>Age 2-12</span>
-          <div className='mt-2'>
+        <div className='section'>
+          <h1 className='title'>Children</h1>
+          <span className='description'>Age 2-12</span>
+          <div className='contador'>
             <button
-              className='mr-2 relative px-1 rounded-md text-gray-800 border border-gray-400 hover:text-gray-600 focus:border-blue-300 focus:outline-none'
+              className='botonContador'
               onClick={() => decreaseGuests('children')}
             >
               －
             </button>
             <span>{children}</span>
             <button
-              className='ml-2 relative px-1 rounded-md text-gray-800 border border-gray-400 hover:text-gray-600 focus:border-blue-300 focus:outline-none'
+              className='botonContador'
               onClick={() => increaseGuests('children')}
             >
               ＋
